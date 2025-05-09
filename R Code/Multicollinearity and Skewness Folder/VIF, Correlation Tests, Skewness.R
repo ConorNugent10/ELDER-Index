94d35b256ff68b1abac9d50ea10dede0f0c280f0
ConorNugent10 <conor10nugent@gmail.com>
2025-05-09 12:15:41 +0100

    Add files via upload

--- R Code/Multicollinearity and Skewness Folder/VIF, Correlation Tests, Skewness.R
+++ R Code/Multicollinearity and Skewness Folder/VIF, Correlation Tests, Skewness.R
@@ -8,13 +8,13 @@ library(moments)
 getwd()
 
 # Load data from  .csv
-df <- read.csv("data.csv")
+data <- read.csv("data.csv")
 
 # Check
-head(df)
+head(data)
 
 # Calculate coefficients and put into dataframe
-correlation_matrix <- cor(df, method = "pearson")
+correlation_matrix <- cor(data, method = "pearson")
 print(correlation_matrix)
 
 # Round values
@@ -25,7 +25,7 @@ print(correlation_table)
 vif_values <- diag(solve(correlation_matrix))
 
 # Then put into dataframe
-vif_df <- data.frame(Variable = colnames(df), VIF = vif_values)
+vif_df <- data.frame(Variable = colnames(data), VIF = vif_values)
 print(vif_df)
 
 # Set colour palette as blue to white to red
@@ -35,5 +35,5 @@ colour_palette <- colorRampPalette(c("#6BAED6", "white", "#FB8072"))(100)
 corrplot(correlation_table, method = "circle", type = "lower", diag = FALSE, col = colour_palette, tl.col = "black", tl.srt = 45, cl.cex = 0.7, addCoef.col = "black", number.cex = 0.7, title = "Correlation Matrix", mar = c(0, 0, 5, 0))     # Title margin
 
 # Get skewness values and put into dataframe
-skew_values <- sapply(df, skewness)
+skew_values <- sapply(data, skewness)
 skew_values 
